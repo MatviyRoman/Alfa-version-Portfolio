242ffea95db383becb91582979ec09880adba79a
Roman <romas6ka@i.ua>
2019-02-22 18:35:57 +0200

    Add files via upload

--- fontawesome/js/v4-shims.js
+++ fontawesome/js/v4-shims.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global['fontawesome-free-shims'] = factory());
+  (global['fontawesome-pro-shims'] = factory());
 }(this, (function () { 'use strict';
 
   var _WINDOW = {};
